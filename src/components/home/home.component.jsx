6afbcd76b7d487d8f9cfbbae7257dbba60279c0f
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-04-16 23:09:20 +0200

    done styling of home page

---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,29 @@
+import './home.styles.css';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1 className='title'>DACHI-GIORGI GARUCHAVA</h1>
+            <ul className="info-links">
+                <li>Contact</li>
+                <li>CV</li>
+            </ul>
+            <ul className="project-links">
+                <li>The Witness</li>
+                <li>Invasive Modification</li>
+                <li>STEPS</li>
+                <li>Hyphen</li>
+                <li>I Need Protection</li>
+                <li>French Lessons</li>
+                <li>Kids of the Pink Trash</li>
+                <li>PSR</li>
+                <li>In Luce Side</li>
+                <li>Tide</li>
+                <li>Bulbosa</li>
+                <li>Barbed wire</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
