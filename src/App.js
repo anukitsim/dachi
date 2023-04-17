616e279b7f10c9794c205635485b12766d679d79
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-04-17 02:38:05 +0200

    made homepage responsive for mobile screen using media quaries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Home from "./components/home/home.component";
+import { Route, Routes } from "react-router-dom";
+
+
+
+
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}>
+        
+        </Route>
+        
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,29 +1,46 @@
+import React, { useState } from 'react';
 import './home.styles.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <h1 className='title'>DACHI-GIORGI GARUCHAVA</h1>
-            <ul className="info-links">
-                <li>Contact</li>
-                <li>CV</li>
-            </ul>
-            <ul className="project-links">
-                <li>The Witness</li>
-                <li>Invasive Modification</li>
-                <li>STEPS</li>
-                <li>Hyphen</li>
-                <li>I Need Protection</li>
-                <li>French Lessons</li>
-                <li>Kids of the Pink Trash</li>
-                <li>PSR</li>
-                <li>In Luce Side</li>
-                <li>Tide</li>
-                <li>Bulbosa</li>
-                <li>Barbed wire</li>
-            </ul>
-        </div>
-    )
-}
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuVisible(!isMobileMenuVisible);
+  };
+
+  return (
+    <div className="home-container">
+      <h1 className="title">
+        <Link to="/" className="title-link">
+          DACHI-GIORGI GARUCHAVA
+        </Link>
+      </h1>
+      <ul className={isMobileMenuVisible ? 'info-links show' : 'info-links'}>
+        <li>Contact</li>
+        <li>CV</li>
+      </ul>
+      <div className={isMobileMenuVisible ? 'mobile-menu visible' : 'mobile-menu'}>
+        <button className="menu-button" onClick={toggleMobileMenu}>
+          Menu
+        </button>
+        <ul className={isMobileMenuVisible ? 'project-links show' : 'project-links'}>
+          <li>The Witness</li>
+          <li>Invasive Modification</li>
+          <li>STEPS</li>
+          <li>Hyphen</li>
+          <li>I Need Protection</li>
+          <li>French Lessons</li>
+          <li>Kids of the Pink Trash</li>
+          <li>PSR</li>
+          <li>In Luce Side</li>
+          <li>Tide</li>
+          <li>Bulbosa</li>
+          <li>Barbed wire</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
