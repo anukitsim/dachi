3f3e9e79af12959962a3b49f3e97cd471fa6bd6e
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-04-18 20:27:57 +0200

    STEPS done for desktop screens

--- src/components/steps/steps.component.jsx
+++ src/components/steps/steps.component.jsx
@@ -1,9 +1,40 @@
-
+import ReactPlayer from "react-player";
+import "./steps.styles.css";
 
 const Steps = () => {
-    return (
-        <div>STEPS</div>
-    )
+  const isMobile = window.innerWidth <= 768; // check if screen size is <= 768px
+
+  return (
+    <div className="video-container">
+      {isMobile ? (
+        <ReactPlayer
+          url="https://vimeo.com/818803846"
+          className="video"
+          controls
+          volume={1}
+          width="100%"
+          height="auto"
+        />
+      ) : (
+        <ReactPlayer
+          url="https://vimeo.com/818803846"
+          className="video"
+          controls
+          volume={1}
+          width="85vw"
+          height="45vw"
+        />
+      )}
+      <div className="caption-container">
+        <p className="steps-caption1">Animated and Produced for</p>
+        <p className="steps-caption2">
+          <a className="music-link" href="https://www.youtube.com/watch?v=lBEGDRO8N2g">
+            Steps ft. MNEK & Saul Williams
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
