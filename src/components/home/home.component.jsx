d74c65e4916849577c49c5df0a748c023725c694
anukitsim <104069419+anukitsim@users.noreply.github.com>
2023-04-19 17:21:23 +0200

    added responsivity

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './home.styles.css';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 
+
+
 const Home = () => {
   const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
   const location = useLocation(); // import useLocation hook from react-router-dom
@@ -35,18 +37,18 @@ const Home = () => {
           <li><Link to='/cv' className='info-link'>CV</Link></li>
         </ul>
         <ul className={isMobileMenuVisible ? 'project-links show' : 'project-links'}>
-          <li><Link to='/thewitness' className='project-link'>The Witness</Link></li>
-          <li><Link to='/invasive-modification' className='project-link'>Invasive Modification</Link></li>
-          <li><Link to='/STEPS' className='project-link'>STEPS</Link></li>
-          <li><Link to='/hyphen' className='project-link'>Hyphen</Link></li>
-          <li><Link to='/i-need-protection' className='project-link'>I Need Protection</Link></li>
-          <li><Link to='/french-lessons' className='project-link'>French Lessons</Link></li>
-          <li><Link to='/kidsofthepinktrash' className='project-link'>Kids of the Pink Trash</Link></li>
-          <li><Link to='/bulbosa' className='project-link'>Bulbosa</Link></li>
-          <li><Link to='/tide' className='project-link'>Tide</Link></li>
-          <li><Link to='/in-luce-side' className='project-link'>in Luce Side</Link></li>
-          <li><Link to='/psr' className='project-link'>PSR</Link></li>
-          <li><Link to='/barbed-wire' className='project-link'>Barbed wire</Link></li>
+        <li><Link to='/thewitness' className={location.pathname === '/thewitness' ? 'project-link current-page' : 'project-link'}>The Witness</Link></li>
+          <li><Link to='/invasive-modification' className={location.pathname === '/invasive-modification' ? 'project-link current-page' : 'project-link'}>Invasive Modification</Link></li>
+          <li><Link to='/STEPS' className={location.pathname === '/STEPS' ? 'project-link current-page' : 'project-link'}>STEPS</Link></li>
+          <li><Link to='/hyphen' className={location.pathname === '/hyphen' ? 'project-link current-page' : 'project-link'}>Hyphen</Link></li>
+          <li><Link to='/i-need-protection' className={location.pathname === '/i-need-protection' ? 'project-link current-page' : 'project-link'}>I Need Protection</Link></li>
+          <li><Link to='/french-lessons' className={location.pathname === '/french-lessons' ? 'project-link current-page' : 'project-link'}>French Lessons</Link></li>
+          <li><Link to='/kidsofthepinktrash' className={location.pathname === '/kidsofthepinktrash' ? 'project-link current-page' : 'project-link'}>Kids of the Pink Trash</Link></li>
+          <li><Link to='/bulbosa' className={location.pathname === '/bulbosa' ? 'project-link current-page' : 'project-link'}>Bulbosa</Link></li>
+          <li><Link to='/tide' className={location.pathname === '/tide' ? 'project-link current-page' : 'project-link'}>Tide</Link></li>
+          <li><Link to='/in-luce-side' className={location.pathname === '/in-luce-side' ? 'project-link current-page' : 'project-link'}>in Luce Side</Link></li>
+          <li><Link to='/psr' className={location.pathname === '/psr' ? 'project-link current-page' : 'project-link'}>PSR</Link></li>
+          <li><Link to='/barbed-wire' className={location.pathname === '/barbed-wire' ? 'project-link current-page' : 'project-link'}>Barbed wire</Link></li>
         </ul>
       </div>
       <Outlet />
